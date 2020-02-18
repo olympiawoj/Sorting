3362643b8c52454137fb4dcc8b6a41a5522cf968
olympia <owojcik7@gmail.com>
2020-02-17 19:15:26 -0500

    Fix bubble_sort implementation

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -4,6 +4,18 @@
 arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
 # len is 10
 """
+
+1. Start with current index = 0
+
+2. For all indices EXCEPT the last index:
+
+    a. Loop through elements on right-hand-side 
+    of current index and find the smallest element
+
+    b. Swap the element at current index with the
+    smallest element found in above loop
+
+
 Selection sort segments the list into two parts - the left most is sorted, the right is unsorted
 
 1. Search through entire list for smallest element
@@ -15,6 +27,7 @@ j = iterate over unsorted items, which are
 
 i 0, check j 1-9 items
 i 1, check j 2-9 items
+
 
 """
 
@@ -42,12 +55,19 @@ print(selection_sort(arr1))
 
 # TO-DO:  implement the Bubble Sort function below
 
+"""
+1. Loop through your array
+- Compare each element to its neighbor
+- If elements in wrong position(relative to each other, swap them)
+2. If no swaps performed, stop. Else, go back to the element at index 0 and repeat step 1.
+"""
+
 
 def bubble_sort(arr):
     # Loop over ever item in the array
     for i in range(0, len(arr)):
-        # Repeat process for every item in array minus 1
-        for j in range(0, len(arr) - i - 1):
+        # For every item in array, compare it to it's neighbhor
+        for j in range(0, len(arr) - 1):
             print(arr, arr[j], arr[j+1])
             if arr[j] > arr[j+1]:
                 # SWAP
